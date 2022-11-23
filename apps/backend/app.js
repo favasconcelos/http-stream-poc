370ae9ef7bb03483841b9886455dd16d6d83bb9a
Felipe Vasconcelos <f@avasconcelos.com>
2022-11-23 22:09:52 +0100

    refactor: replace 'var' in favor of 'const'

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -1,12 +1,12 @@
-var createError = require("http-errors");
-var express = require("express");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var cors = require("cors");
+const createError = require("http-errors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-var indexRouter = require("./routes/index");
+const indexRouter = require("./routes/index");
 
-var app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
--- apps/backend/routes/index.js
+++ apps/backend/routes/index.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 function sleep(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
